88020bd1dd0079d3948411d0506b166e2fc099f4
Mohammad Mohsen-Pour <mohammad.mohsenpour1@gmail.com>
2020-06-07 14:04:20 -0400

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,10 +1,4 @@
 const helloWorld = () => {
-<<<<<<< HEAD
-  console.log("added Sarah's line");
-=======
-  console.log("sarah's line");
-  console.log("john's line");
->>>>>>> cf8e2a36f3eaf38dc23c4d282448d0156442eb6d
 }
 
 helloWorld();
